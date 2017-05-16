8f8c58edf078d970178817906b015743e4c2f775
fish <luk1684tw@gmail.com>
2017-05-17 01:06:54 +0800

    [CSS]Change UI looks

--- virpet-client/src/components/Main.jsx
+++ virpet-client/src/components/Main.jsx
@@ -67,8 +67,9 @@ class Main extends React.Component {
                                     <SingleEvent/>
                                     &nbsp;&nbsp;
                                     <Login/>
+                                    &nbsp;&nbsp;
                                     <div>
-                                        <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="Login with Google" onSuccess={responseGoogle} onFailure={responseGoogle} className='btn btn-secondary' offline={false}></GoogleLogin>
+                                        <GoogleLogin clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" buttonText="GoogleLogin" onSuccess={responseGoogle} onFailure={responseGoogle} className='btn btn-primary' offline={false}></GoogleLogin>
                                     </div>
                                     <div className='search ml-auto'>
                                         <Input className='ml-auto' type='text' placeholder='Search' onKeyPress={this.handleSearchKeyPress} getRef={e => this.searchEl = e}></Input>
